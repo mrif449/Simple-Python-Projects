32426a330fbe18fe1e5be5968b522e57253536dc
Md. Rahadul Islam Fardin <85452696+mrif449@users.noreply.github.com>
2022-01-29 03:36:08 -0800

    Add files via upload

---
+++ Tic Tac Toe/main.py
@@ -0,0 +1,185 @@
+space = "   "
+print("****Welcome to Tic-Tac-Toe****")
+
+print(" 0 | 1 | 2 \n----------\n 3 | 4 | 5 \n----------\n 6 | 7 | 8 ")
+x = " X "
+o = " O "
+empty = "   "
+array = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+for i in range(9):
+    if i == 0:
+            print("Your Turn: ")
+            print()
+            command = int(input('Enter the index: '))
+            array[command] = o
+            print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+            if command != 4:
+                array[4] = x
+            else:
+                array[0] = x
+            print("Computer's turn: ")
+            print()
+            print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+            print()
+    elif i == 1:
+        print("Your Turn: ")
+        print()
+        command = int(input('Enter the index: '))
+        array[command] = o
+        print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+        if ((array[1] == o and array[3] == o) or (array[2] == o and array[3] == o)):
+            array[0] = x
+        elif (array[2] == o and array[6] == o):
+            array[1] = x
+        elif (array[4] == o and array[8] == o) or (array[0] == o and array[5] == o) or (array[1] == o and array[6] == o) or (array[1] == o and array[8] == o) or (array[1] == o and array[5] == o):
+            array[2] = x
+        elif (array[0] == o and array[7] == o) or (array[3] == o and array[8] == o) or (array[3] == o and array[7] == o):
+            array[6] = x
+        elif (array[2] == o and array[7] == o) or (array[5] == o and array[6] == o) or (array[5] == o and array[7] == o):
+            array[8] = x
+        else:
+            for z in range(len(array)):
+                if array[z] == space:
+                    array[z] = x
+                    break 
+        print("Computer's turn: ")
+        print()
+        print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+        print()
+    elif i == 2:
+        print("Your Turn: ")
+        print()
+        command = int(input('Enter the index: '))
+        array[command] = o
+        print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+        if (array[0] == o and array[4] == o and array[8] == o) or (array[0] == o and array[1] == o and array[2] == o) or (array[0] == o and array[3] == o and array[6] == o) or (array[1] == o and array[4] == o and array[7] == o) or (array[2] == o and array[4] == o and array[6] == o) or (array[3] == o and array[4] == o and array[5] == o) or (array[6] == o and array[7] == o and array[8] == o):
+            print("You Win!!!")
+            break
+        elif ((array[1] == o and array[2] == o) or (array[3] == o and array[6] == o) or (array[4] == o and array[8] == o)) and (array[0] == space):
+                array[0] = x
+        elif ((array[0] == o and array[2] == o) or (array[4] == o and array[7] == o)) and (array[1] == space):
+                array[1] = x
+        elif ((array[0] == o and array[1] == o) or (array[4] == o and array[6] == o) or (array[5] == o and array[8] == o)) and (array[2] == space):
+            array[2] = x
+        elif ((array[0] == o and array[6] == o) or (array[4] == o and array[5] == o)) and (array[3] == space):
+                array[3] = x
+        elif ((array[0] == o and array[8] == o) or (array[1] == o and array[7] == o) or (array[2] == o and array[6] == o) or (array[3] == o and array[5] == o)) and (array[4] == space):
+                array[4] = x
+        elif ((array[3] == o and array[4] == o) or (array[2] == o and array[8] == o)) and (array[5] == space):
+                array[5] = x
+        elif ((array[0] == o and array[3] == o) or (array[2] == o and array[4] == o) or (array[7] == o and array[8] == o)) and (array[6] == space):
+                array[6] = x
+        elif ((array[1] == o and array[4] == o) or (array[6] == o and array[8] == o)) and (array[7] == space):
+            array[7] = x
+        elif ((array[0] == o and array[4] == o) or (array[2] == o and array[5] == o) or (array[6] == o and array[7] == o)) and (array[8] == space):
+                array[8] = x
+        elif ((array[1] == x and array[2] == x) or (array[3] == x and array[6] == x) or (array[4] == x and array[8] == x)) and (array[0] == space):
+                array[0] = x
+        elif ((array[0] == x and array[2] == x) or (array[4] == x and array[7] == x)) and (array[1] == space):
+                array[1] = x
+        elif ((array[0] == x and array[1] == x) or (array[4] == x and array[6] == x) or (array[5] == x and array[8] == x)) and (array[2] == space):
+            array[2] = x
+        elif ((array[0] == x and array[6] == x) or (array[4] == x and array[5] == x)) and (array[3] == space):
+                array[3] = x
+        elif ((array[0] == x and array[8] == x) or (array[1] == x and array[7] == x) or (array[2] == x and array[6] == x) or (array[3] == x and array[5] == x)) and (array[4] == space):
+                array[4] = x
+        elif ((array[3] == x and array[4] == x) or (array[2] == x and array[8] == x)) and (array[5] == space):
+                array[5] = x
+        elif ((array[0] == x and array[3] == x) or (array[2] == x and array[4] == x) or (array[7] == x and array[8] == x)) and (array[6] == space):
+                array[6] = x
+        elif ((array[1] == x and array[4] == x) or (array[6] == x and array[8] == x)) and (array[7] == space):
+            array[7] = x
+        elif ((array[0] == x and array[4] == x) or (array[2] == x and array[5] == x) or (array[6] == x and array[7] == x)) and (array[8] == space):
+                array[8] = x
+        else:
+            for z in range(len(array)):
+                if array[z] == space:
+                    array[z] = x
+                    break
+        print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+        if (array[0] == x and array[4] == x and array[8] == x) or (array[0] == x and array[1] == x and array[2] == x) or (array[0] == x and array[3] == x and array[6] == x) or (array[1] == x and array[4] == x and array[7] == x) or (array[2] == x and array[4] == x and array[6] == x) or (array[3] == x and array[4] == x and array[5] == x) or (array[3] == x and array[4] == x and array[5] == x) or (array[6] == x and array[7] == x and array[8] == x):
+            print("Computer's turn: ")
+            print()
+            print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+            print("Computer Wins!!!")
+            break
+    elif i == 3:
+        print("Your Turn: ")
+        print()
+        command = int(input('Enter the index: '))
+        array[command] = o
+        print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+        print()
+        if (array[0] == o and array[4] == o and array[8] == o) or (array[0] == o and array[1] == o and array[2] == o) or (array[0] == o and array[3] == o and array[6] == o) or (array[1] == o and array[4] == o and array[7] == o) or (array[2] == o and array[4] == o and array[6] == o) or (array[3] == o and array[4] == o and array[5] == o) or (array[6] == o and array[7] == o and array[8] == o) or (array[5] == o and array[2] == o and array[8] == o):
+            print("You Win!!!")
+            break
+        elif ((array[1] == o and array[2] == o) or (array[3] == o and array[6] == o) or (array[4] == o and array[8] == o)) and (array[0] == space):
+                array[0] = x
+        elif ((array[0] == o and array[2] == o) or (array[4] == o and array[7] == o)) and (array[1] == space):
+                array[1] = x
+        elif ((array[0] == o and array[1] == o) or (array[4] == o and array[6] == o) or (array[5] == o and array[8] == o)) and (array[2] == space):
+            array[2] = x
+        elif ((array[0] == o and array[6] == o) or (array[4] == o and array[5] == o)) and (array[3] == space):
+                array[3] = x
+        elif ((array[0] == o and array[8] == o) or (array[1] == o and array[7] == o) or (array[2] == o and array[6] == o) or (array[3] == o and array[5] == o)) and (array[4] == space):
+                array[4] = x
+        elif ((array[3] == o and array[4] == o) or (array[2] == o and array[8] == o)) and (array[5] == space):
+                array[5] = x
+        elif ((array[0] == o and array[3] == o) or (array[2] == o and array[4] == o) or (array[7] == o and array[8] == o)) and (array[6] == space):
+                array[6] = x
+        elif ((array[1] == o and array[4] == o) or (array[6] == o and array[8] == o)) and (array[7] == space):
+            array[7] = x
+        elif ((array[0] == o and array[4] == o) or (array[2] == o and array[5] == o) or (array[6] == o and array[7] == o)) and (array[8] == space):
+                array[8] = x
+        elif ((array[1] == x and array[2] == x) or (array[3] == x and array[6] == x) or (array[4] == x and array[8] == x)) and (array[0] == space):
+                array[0] = x
+        elif ((array[0] == x and array[2] == x) or (array[4] == x and array[7] == x)) and (array[1] == space):
+                array[1] = x
+        elif ((array[0] == x and array[1] == x) or (array[4] == x and array[6] == x) or (array[5] == x and array[8] == x)) and (array[2] == space):
+            array[2] = x
+        elif ((array[0] == x and array[6] == x) or (array[4] == x and array[5] == x)) and (array[3] == space):
+                array[3] = x
+        elif ((array[0] == x and array[8] == x) or (array[1] == x and array[7] == x) or (array[2] == x and array[6] == x) or (array[3] == x and array[5] == x)) and (array[4] == space):
+                array[4] = x
+        elif ((array[3] == x and array[4] == x) or (array[2] == x and array[8] == x)) and (array[5] == space):
+                array[5] = x
+        elif ((array[0] == x and array[3] == x) or (array[2] == x and array[4] == x) or (array[7] == x and array[8] == x)) and (array[6] == space):
+                array[6] = x
+        elif ((array[1] == x and array[4] == x) or (array[6] == x and array[8] == x)) and (array[7] == space):
+            array[7] = x
+            
+        elif ((array[0] == x and array[4] == x) or (array[2] == x and array[5] == x) or (array[6] == x and array[7] == x)) and (array[8] == space):
+                array[8] = x
+        else:
+            for z in range(len(array)):
+                if array[z] == space:
+                    array[z] = x
+                    break
+        if (array[0] == x and array[4] == x and array[8] == x) or (array[0] == x and array[1] == x and array[2] == x) or (array[0] == x and array[3] == x and array[6] == x) or (array[1] == x and array[4] == x and array[7] == x) or (array[2] == x and array[4] == x and array[6] == x) or (array[3] == x and array[4] == x and array[5] == x) or (array[3] == x and array[4] == x and array[5] == x) or (array[6] == x and array[7] == x and array[8] == x):
+            print("Computer's turn: ")
+            print()
+            print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+            print("Computer Wins!!!")
+            break
+        if (array[0] == o and array[3] == o and array[2] == o and array[7] == o):
+            print("Computer's turn: ")
+            print()
+            print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+            print("DRAW!!!")
+            break
+        print("Computer's turn: ")
+        print()
+        print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+    elif i == 4:
+        print("Your Turn: ")
+        print()
+        command = int(input('Enter the index: '))
+        array[command] = o
+        print(f"{array[0]}|{array[1]}|{array[2]}\n----------\n{array[3]}|{array[4]}|{array[5]}\n----------\n{array[6]}|{array[7]}|{array[8]}")
+        print()
+        if (array[0] == o and array[4] == o and array[8] == o) or (array[0] == o and array[1] == o and array[2] == o) or (array[0] == o and array[3] == o and array[6] == o) or (array[1] == o and array[4] == o and array[7] == o) or (array[2] == o and array[4] == o and array[6] == o) or (array[3] == o and array[4] == o and array[5] == o) or (array[6] == o and array[7] == o and array[8] == o) or (array[6] == o and array[7] == o and array[8] == o):
+            print("You Win!!!")
+            break
+        else:
+            print("DRAW!!!")
+command3 = input("Press Enter to close...")
